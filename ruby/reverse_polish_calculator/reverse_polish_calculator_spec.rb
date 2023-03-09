2bbb8c29285a189cd34ba39b2f25faff71d6889f
Andrew Mullins <mullinsand15@gmail.com>
2023-03-09 06:13:53 -0700

    updated guard clause for negative operations

--- ruby/reverse_polish_calculator/reverse_polish_calculator_spec.rb
+++ ruby/reverse_polish_calculator/reverse_polish_calculator_spec.rb
@@ -16,5 +16,15 @@ RSpec.describe ReversePolishCalculator do
       input = '10 6 9 3 + -11 * / * 17 + 5 +'
       expect(ReversePolishCalculator.evaluate_string(input)).to eq(22)
     end
+
+    it 'solves 2 5 * 4 + 3 2 * 1 + / string' do
+      input = '2 5 * 4 + 3 2 * 1 + /'
+      expect(ReversePolishCalculator.evaluate_string(input)).to eq(2)
+    end
+
+    it 'solves 3 2 * 11 - string' do
+      input = '3 2 * 11 -'
+      expect(ReversePolishCalculator.evaluate_string(input)).to eq(-5)
+    end
   end
 end
